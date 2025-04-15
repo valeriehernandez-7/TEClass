dcecbcf849b2d9b4090f4b417165afd1cf529fc1
William Jiménez <williamjimv668@gmail.com>
2025-04-15 12:22:25 -0600

    Carpetas de Conexión

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const uri = 'mongodb+srv://wjv668:668@schoolify.z67yugl.mongodb.net/?retryWrites=true&w=majority&appName=Schoolify';
+const uri = 'mongodb+srv://<user>:<password>@schoolify.z67yugl.mongodb.net/?retryWrites=true&w=majority&appName=Schoolify';
 const client = new MongoClient(uri);
 
 async function run() {
