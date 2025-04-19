037c534e9b33de8a4c1088e0f7f5758999e8754b
William Jiménez <williamjimv668@gmail.com>
2025-04-19 17:44:26 -0600

    Update m_functions.js

--- src/databases/mongo/m_functions.js
+++ src/databases/mongo/m_functions.js
@@ -79,6 +79,7 @@ async function validateUser(username, plainPassword) {
             id: user.id,
             username: user.username,
             first_name: user.first_name,
+            last_name: user.last_name,
             avatar_url: user.avatar_url,
             role: user.role
         };
