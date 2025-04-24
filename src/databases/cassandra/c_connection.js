de4eaa507f2a8fb108e5074a9e771464b0b48767
William Jiménez <williamjimv668@gmail.com>
2025-04-24 02:05:34 -0600

    Enrollment

--- src/databases/cassandra/c_connection.js
+++ src/databases/cassandra/c_connection.js
@@ -1,12 +1,12 @@
 const cassandra = require('cassandra-driver');
 
-const client = new cassandra.Client({
-  contactPoints: ['localhost'],
-  localDataCenter: 'datacenter1', 
-  keyspace: 'proyectodb2'
-});
-
 async function connectCassandra() {
+  const client = new cassandra.Client({
+    contactPoints: ['127.0.0.1', '127.0.0.2'],
+    localDataCenter: 'datacenter1',
+    keyspace: 'proyectobd2'
+  });
+
   await client.connect();
   return client;
 }
--- src/databases/cassandra/c_routes.js
+++ src/databases/cassandra/c_routes.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const { enrollUserInCourse } = require('../controllers/enrollmentController');
 
+router.post('/enroll-by-user', async (req, res) => {
+  const { userId, courseId } = req.body;
 
+  try {
+    await enrollUserInCourse(userId, courseId);
+    return res.status(200).send('Matrícula registrada correctamente');
+  } catch (error) {
+    console.error('Error al matricular:', error);
+    return res.status(500).send('Error al matricular');
+  }
+});
 
 module.exports = router;
